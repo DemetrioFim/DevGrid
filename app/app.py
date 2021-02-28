1d6c347a52f3c9fe9a9c98c02d7f7722bb60b91b
demetrio_bknp <demetrio@bknp.com.br>
2021-02-28 01:50:02 -0300

    Inserted the input, the response in html and python

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<city>')
-def main_page(city=None):
-    return render_template('main_page.html', city=city)
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'GET':
+        return render_template('main_page.html')
+    elif request.method == 'POST':
+        city = request.form['input_city']
+        return render_template('main_page.html', city=city)
+
+
+
 
 if __name__ == "__main__":
     app.run()
