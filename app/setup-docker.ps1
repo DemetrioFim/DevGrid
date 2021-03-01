b6df1e655d919f19a889dd69d3bc2e5669490133
demetrio.fim@gmail.com <demetrio@bknp.com.br>
2021-03-01 02:37:59 -0300

    dockerized app

---
+++ app/setup-docker.ps1
@@ -0,0 +1,7 @@
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+Set-Location $dir
+
+docker build -t weather-buddy .
+docker run --name weather-buddy-app -p 5000:5000 weather-buddy
